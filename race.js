de651fac452f2c47ff0117fb4cf8d849941dd386
Alexandra V <78971710+alexandravladu@users.noreply.github.com>
2022-06-15 15:33:58 +0100

    Create race.js
    Runners are assigned a race number and start time-based on their age and registration.

---
+++ race.js
@@ -0,0 +1,26 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = true;
+const age = 22;
+if (registeredEarly && age >18){
+  raceNumber =+ 1000;
+}
+if (registeredEarly && age >18){
+  console.log(`Race will begin at 9:30, your race number is ${raceNumber}.`);
+}
+else if (!registeredEarly && age > 18){
+  console.log(`Race will beging at 11:00, your race number is ${raceNumber}.`);
+}
+else if (age<18) {
+ console.log(`Race will beging at 12:30, your race number is ${raceNumber}.`);
+}
+else{
+  console.log('Please approach the registration desk, thanks!');
+}
+
+
+
+
+
+
+
+
