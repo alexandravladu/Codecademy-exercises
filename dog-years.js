ae38be8d59b3674e572b92d199209dd8a9c01885
Alexandra V <78971710+alexandravladu@users.noreply.github.com>
2022-06-15 15:30:22 +0100

    Create dog-years.js
    A simple exercise to calculate my age in dog years.

---
+++ dog-years.js
@@ -0,0 +1,12 @@
+//my age-a constant variable
+const myAge = 22;
+//a new variable which will have to be changed later
+let earlyYears=2;
+earlyYears *=10.5;
+ let laterYears = myAge-2;
+ //multiplication
+ laterYears *= 4;
+ let myAgeInDogYears = earlyYears + laterYears;
+ let myName = 'Alexandra';
+ 'Alexandra'.toLowerCase();
+ console.log(`My name is ${myName}. I am ${myAge} old in human years which is ${myAgeInDogYears} years old in dog years.`);
